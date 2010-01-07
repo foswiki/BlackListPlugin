4745629312f1535b0b047f4f66596d9c9dd47667
KennethLavrsen <KennethLavrsen@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-01-07 23:13:55 +0000

    Item2577: Finish the https report for release I open a new for the performance with many links in topic part
    git-svn-id: http://svn.foswiki.org/trunk/BlackListPlugin@5969 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/BlackListPlugin.pm
+++ lib/Foswiki/Plugins/BlackListPlugin.pm
@@ -49,7 +49,7 @@ our $VERSION = '$Rev$';
 # This is a free-form string you can use to "name" your own plugin version.
 # It is *not* used by the build automation tools, but is reported as part
 # of the version number in PLUGINDESCRIPTIONS.
-our $RELEASE = '04 Jan 2010';
+our $RELEASE = '08 Jan 2010';
 
 our $pluginName = 'BlackListPlugin';  # Name of this Plugin
 our %cfg =
